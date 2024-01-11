6a33f777c690b3ead5fe9adb92f6f31a689a1971
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2024-01-11 07:11:37 -0600

    add linked in url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,13 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;<code className={styles.code}>src/app/page.tsx</code>
-        </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="www.linkedin.com/in/iriosc0900"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By Michelle Rios
           </a>
         </div>
       </div>
