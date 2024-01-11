d92c2bb9e866556e7c55a97c7218cb515ec15cf4
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2024-01-11 06:43:34 -0600

    Updated dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Get started by editing&nbsp;<code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
           <a
